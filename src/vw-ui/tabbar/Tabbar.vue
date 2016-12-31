50e48e4d147e1bfd359240058e59233caaa6e101
Jay.M.Hu <hm910705@163.com>
2016-12-31 21:53:47 +0800

    Add vw-ui components

---
+++ src/vw-ui/tabbar/Tabbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="weui-tabbar">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'vw-tabbar',
+    props: {
+      value: null
+    }
+  };
+</script>
