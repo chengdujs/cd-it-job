fbedbba6b76fbe94c37887d3fa6145c4e0ed7cb4
mabelyong <mabelyong@163.com>
2017-01-17 11:46:51 +0800

    mod funtion name

--- src/common/formatDate.js
+++ src/common/formatDate.js
@@ -22,13 +22,13 @@ export function formatDate(date, fmt) {
   for (let k in o) {
     if (new RegExp(`(${k})`).test(fmt)) {
       let str = o[k] + '';
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : PadLeftZero(str))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
     }
   };
 
   return fmt;
 }
 
-function PadLeftZero(str) {
+function padLeftZero(str) {
   return ('00' + str).substr(str.length)
 };
