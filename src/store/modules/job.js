2d79dd508e764953bdbcf804e71d16c4ea6d77bd
mabelyong <mabelyong@163.com>
2017-01-19 02:13:21 +0800

    add note

--- src/store/modules/job.js
+++ src/store/modules/job.js
@@ -8,12 +8,19 @@ const state = {
   jobs: []
 };
 
+/**
+ * 提交同步
+ */
 const mutations = {
   [JOB.GET_JOBS](state, data) {
     state.jobs = data;
   }
 };
 
+/**
+ * 提交异步请求
+ * @param 与store实例具有相同方法和属性的 context 对象
+ */
 const actions = {
   [JOB.GET_JOBS]({ commit }) {
     return ajax.get('http://chat.hstar.org:8601/e2607fcaf7fe/employmentList')
