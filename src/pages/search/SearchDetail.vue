b5fe4861586ed38be67100d42a8899e7dae86ecc
Treasure <treasurewmj@foxmail.com>
2017-01-18 15:05:45 +0800

    append GPL

---
+++ src/pages/search/SearchDetail.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name:search-detail,
+  data(){
+    return{
+      comDetail:{} //companyDetail
+    }
+  },
+  props(){
+
+  },
+  created(){
+    ajax.get('/static/mock/companyDetail.json')
+      .then(data => {
+        this.comDetail = data;
+      });
+  },
+}
+</script>
+
+<style lang="css">
+</style>
