b3426c78153aff3d2f314cb4bd80507c1fc4034c
zerom <zero@zerosrat.com>
2017-02-13 15:09:36 +0800

    图片上传组件完成

--- src/pages/my/MyPage.vue
+++ src/pages/my/MyPage.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="page-my">
     <h1>My</h1>
+    <img-upload :imgSrc="src" @imgChange="imgChange"></img-upload>
   </div>
 </template>
+
+<script>
+import { imgUpload } from 'components';
+
+export default {
+  components: {
+    imgUpload
+  },
+  data() {
+    return {
+      src: 'http://7xoxnz.com1.z0.glb.clouddn.com/classAdapter.png'
+    }
+  },
+  methods: {
+    imgChange(src) {
+      this.src = src;
+    }
+  }
+}
+</script>
